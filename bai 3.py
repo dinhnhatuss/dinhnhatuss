8aa57127c7e702ed277cd0c8dac21c873d0aff45
dinhnhatuss <83196740+dinhnhatuss@users.noreply.github.com>
2021-05-08 10:20:53 +0700

    Add files via upload

---
+++ bai 3.py
@@ -0,0 +1,9 @@
+import math;
+x1=int(input("nhap vào x1"))
+y1= int(input("nhap vao y1"))
+x2=int(input("nhap vao x2"))
+y2=int(input("nh?p vào y2"))
+d1=(x2-x1)^2
+d2=(y2-y1)^2
+res= math.sqrt(d1+d2)
+print("khoang cach 2 diem la:",res)
